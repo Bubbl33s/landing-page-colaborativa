fb7594cfa19466e48fa67b746d78942d24186b93
AAnet07 <alexiavillnetp.2020@gmail.com>
2024-10-15 21:09:42 -0500

    evento al boton

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,9 @@
 console.log("hola,bienvenidx");
 console.log("miau");
+
+const boton = document.getElementById("btn");
+//Agragar un evento
+
+boton.addEventListener("click", function () {
+  console.log("¡Los gatos son adorables!");
+});
